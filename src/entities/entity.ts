6c300afe763cd18f3eb65dd0f04f624dfcfecca7
Ali-Akber Raza <aaoblivion@gmail.com>
2024-12-05 10:46:39 -0500

    test

---
+++ src/entities/entity.ts
@@ -0,0 +1,18 @@
+class entity {
+  private _healthPoints: number;
+  private _movementSpeed: number;
+  private _currentWeapon: Weapon;
+  public get movementSpeed(): number {
+    return this.healthPoints;
+  }
+  public get healthPoints(): number {
+    return this.healthPoints;
+  }
+  public dealDamage(damageValue: number): void {
+
+  }
+}
+
+class Weapon {
+
+}
