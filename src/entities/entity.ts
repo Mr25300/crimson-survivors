5ea5df09fdeef6b7d85745f68d9e83a7aec21a04
Ali-Akber Raza <aaoblivion@gmail.com>
2024-12-06 11:47:06 -0500

    test

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -1,6 +1,8 @@
 import {Weapon} from './weapon';
+import {Vector2} from './../util/vector2';
 export class Entity {
   private _currentMovementSpeed: number;
+  private moveDirection: Vector2;
   private _currentVelocity: Vector2;
   constructor(
     private _healthPoints: number,
@@ -10,6 +12,12 @@ export class Entity {
     this._currentMovementSpeed = _defaultMovementSpeed;
     this._currentVelocity.x = 0;
     this._currentVelocity.y = 0;
+    this.update(0);
+  }
+  public update(deltaTime: number){
+    let targetPostion = this._currentPositionVector;
+    // math bs
+    this._currentPositionVector = targetPostion; // upddated final target position
   }
   public get currentVelocity(): Vector2 {
     return this._currentVelocity;
@@ -29,4 +37,8 @@ export class Entity {
   public resetSpeed(): void {
     this._currentMovementSpeed = this._defaultMovementSpeed;
   }
+}
+export class Player extends Entity{
+  
+  
 }
