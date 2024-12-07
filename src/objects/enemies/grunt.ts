0fb847b69ee88700a40abf563119f1a8e9f951f8
AR <53275317+AAR072@users.noreply.github.com>
2024-12-07 16:49:44 -0500

    added radius

--- src/objects/enemies/grunt.ts
+++ src/objects/enemies/grunt.ts
@@ -3,6 +3,7 @@ import {Vector2} from '../../util/vector2.js';
 import {Entity} from '../entity.js';
 
 export class Grunt extends Entity {
+  private detectionRadius: number = 5;
   constructor(
     position: Vector2,
     public sprite: SpriteModel
@@ -14,7 +15,14 @@ export class Grunt extends Entity {
     this.animationState = 'idle';
   }
   public pathFind(playerLocation: Vector2): void {
-    this.setFaceDirection(playerLocation.subtract(this.position).unit());
-    this.setMoveDirection(playerLocation.subtract(this.position).unit());
+    // if in range
+    if (
+      playerLocation.subtract(this.position).magnitude() <= this.detectionRadius
+    ) {
+      this.setFaceDirection(playerLocation.subtract(this.position).unit());
+      this.setMoveDirection(playerLocation.subtract(this.position).unit());
+    } else {
+      this.setMoveDirection(new Vector2(0, 0));
+    }
   }
 }
